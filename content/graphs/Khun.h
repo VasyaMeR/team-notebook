d162df0857b92fb7ee2cd98d1f99e5d22e3ac2e8
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2023-09-25 22:00:07 +0300

    Create Khun.h

---
+++ content/graphs/Khun.h
@@ -0,0 +1,38 @@
+graph<int> g;
+vector<int> mt, used, rev_mt;
+bool dfs(int v) {
+    if (used[v] == 1)
+        return false;
+    used[v] = 1;
+    for (auto to : g[v]) {
+        if (mt[to] == -1) {
+            rev_mt[v] = to;
+            mt[to] = v;
+            return true;
+        }
+    }
+    for (auto to : g[v]) {
+        if (dfs(mt[to])) {
+            rev_mt[v] = to;
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+void pair_matching() {
+    for (int it = 0; ; it++) {
+        bool finded = false;
+        used.assign(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (rev_mt[i] == -1 && dfs(i)) {
+                cnt++;
+                finded = true;
+            }
+        }
+        if (!finded) {
+            break;
+        }
+    }
+}
