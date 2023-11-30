1c536ff74976debb34371d2ce69ee1f737ee1118
VasyaMer <merenych3@gmail.com>
2023-11-30 10:06:19 +0200

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,110 +1,96 @@
-#ifdef __APPLE__
-#define _GLIBCXX_DEBUG
-#define _GLIBCXX_DEBUG_PEDANTIC
-#else
-#pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-#endif
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
+// #pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
 
+// #define _GLIBCXX_DEBUG
+// #define _GLIBCXX_DEBUG_PEDANTIC
+
+#include <cassert>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <map>
 #include <set>
+#include <array>
+#include <numeric>
 #include <queue>
 #include <deque>
 #include <cmath>
 #include <climits>
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/rope>
 
-#ifdef __APPLE__
-template <typename T>
-class ordered_set : public std::set<T> {
-public:
-    auto find_by_order(size_t order) const {
-        auto cur = this->begin();
-        while (order--) {
-            cur++;
-        }
-        return cur;
-    }
-
-    int order_of_key(const T &key) const {
-        int cnt = 0;
-        for (auto it = this->begin(); it != this->begin(); it++, cnt++) {
-            if (*it == key)
-                return cnt;
-        }
-        return cnt;
-    }
-};
-#else
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, std::less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#endif
-
+// using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
 using namespace std;
 
+
 const int MOD = 998244353;
+const long double PI = 3.141592653589793;
 using ll = long long;
 const ll INF = 1e18;
 
 // #define int ll
 
-template <typename T>
+// template<typename T>
+// using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+// template<typename K, typename V>
+// using hash_table = cc_hash_table<K, V, hash<K>>;
+
+template<typename T>
 using graph = vector<vector<T>>;
 
-template <typename T>
-istream &operator>>(istream &in, vector<T> &a) {
-    for (auto &i : a) {
+template<typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (auto& i: a) {
         in >> i;
     }
     return in;
 }
 
-template <typename T>
-ostream &operator<<(ostream &out, vector<T> &a) {
-    for (auto &i : a) {
+template<typename T>
+ostream& operator<<(ostream& out, vector<T>& a) {
+    for (auto& i: a) {
         out << i << " ";
     }
     return out;
 }
 
-int fast_pow(int a, int b, int mod) {
+ll fast_pow(ll a, ll b, ll mod) {
     if (b == 0)
         return 1;
     if (b % 2) {
-        return (a * fast_pow(a, b - 1, mod)) % mod;
+        return (1ll * a * fast_pow(a, b - 1, mod)) % mod;
     }
-    int k = fast_pow(a, b / 2, mod);
-    return (k * k) % mod;
+    ll k = fast_pow(a, b / 2, mod);
+    return (1ll * k * k) % mod;
 }
 
-int fast_pow(int a, int b) {
+ll fast_pow(ll a, ll b) {
     if (b == 0)
         return 1;
     if (b % 2) {
-        return (a * fast_pow(a, b - 1));
+        return (1ll * a * fast_pow(a, b - 1));
     }
-    int k = fast_pow(a, b / 2);
-    return (k * k);
+    ll k = fast_pow(a, b / 2);
+    return (1ll * k * k);
 }
 
 void solve() {
-
+    
 }
 
-int32_t main(int32_t argc, const char *argv[]) {
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(0);
+int32_t main(int32_t argc, const char * argv[]) {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
     // insert code here...
-    int tt = 1;
+    int tt= 1;
     // std::cin >> tt;
     while (tt--) {
         solve();
