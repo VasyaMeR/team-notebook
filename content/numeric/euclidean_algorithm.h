88ff5d5ac7a8010d65b4e0276c86eb7b9419a6a2
Hroskopf <48129217+Hroskopf@users.noreply.github.com>
2023-12-07 14:04:53 +0100

    Add files via upload
    added extended euclidean algorithm.

---
+++ content/numeric/euclidean_algorithm.h
@@ -0,0 +1,14 @@
+#include <tuple>
+
+template<typename T>
+std::pair<T, T> euclidean_algorithm(T a, T b) {
+    T x = 1, y = 0;
+    T x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+        T q = a1 / b1;
+        std::tie(x, x1) = std::make_tuple(x1, x - q * x1);
+        std::tie(y, y1) = std::make_tuple(y1, y - q * y1);
+        std::tie(a1, b1) = std::make_tuple(b1, a1 - q * b1);
+    }
+    return {x, y};
+}
