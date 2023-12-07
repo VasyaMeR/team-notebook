e0fd98d55f5b78c2904c98c9ef181a4bc93bf342
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2023-12-07 15:14:15 +0200

    Create chinese_remainder_theorem.h

---
+++ content/numeric/chinese_remainder_theorem.h
@@ -0,0 +1,29 @@
+/**
+ * Author: Vasyl Merenych
+ * Date: 2023-12-07
+ * License: CC0
+ * Source: cp-algorithms
+ * Description: Solves chinese remainder theorem. 
+ * Time: O(n), n - number of equations.
+ * Status: -
+ */
+
+struct Congruence {
+    long long a, m;
+};
+
+long long chinese_remainder_theorem(vector<Congruence> const& congruences) {
+    long long M = 1;
+    for (auto const& congruence : congruences) {
+        M *= congruence.m;
+    }
+
+    long long solution = 0;
+    for (auto const& congruence : congruences) {
+        long long a_i = congruence.a;
+        long long M_i = M / congruence.m;
+        long long N_i = mod_inv(M_i, congruence.m);
+        solution = (solution + a_i * M_i % M * N_i) % M;
+    }
+    return solution;
+}
