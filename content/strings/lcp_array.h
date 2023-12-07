1a2f96512ea5d0f946c50ada0cc67292277c421c
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2023-12-07 14:49:18 +0200

    Create lcp_array.h

---
+++ content/strings/lcp_array.h
@@ -0,0 +1,40 @@
+/**
+ * Author: Vasyl Merenych
+ * Date: 2023-12-07
+ * License: CC0
+ * Source: Largest common prefix of substrings. Given a string s of length n, it returns the LCP array of s. Here, the LCP array of s is the array of length n−1, such that the i-th element is the length of the LCP (Longest Common Prefix) of s[sa[i]..n) and s[sa[i+1]..n).
+ * Time: O(N), N - size of string s
+ * Status: -
+ */
+
+template <class T>
+std::vector<int> lcp_array(const std::vector<T>& s,
+                           const std::vector<int>& sa) {
+    int n = int(s.size());
+    assert(n >= 1);
+    std::vector<int> rnk(n);
+    for (int i = 0; i < n; i++) {
+        rnk[sa[i]] = i;
+    }
+    std::vector<int> lcp(n - 1);
+    int h = 0;
+    for (int i = 0; i < n; i++) {
+        if (h > 0) h--;
+        if (rnk[i] == 0) continue;
+        int j = sa[rnk[i] - 1];
+        for (; j + h < n && i + h < n; h++) {
+            if (s[j + h] != s[i + h]) break;
+        }
+        lcp[rnk[i] - 1] = h;
+    }
+    return lcp;
+}
+
+std::vector<int> lcp_array(const std::string& s, const std::vector<int>& sa) {
+    int n = int(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) {
+        s2[i] = s[i];
+    }
+    return lcp_array(s2, sa);
+}
