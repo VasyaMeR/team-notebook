5cfa79f283950fc3bd69ef71bca2c61090bdd93b
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2023-12-15 14:06:36 +0200

    Fix z_function.h

--- content/strings/z_function.h
+++ content/strings/z_function.h
@@ -12,14 +12,16 @@ vector<int> z_function(const string& s) {
 	int n = s.size();
 	vector<int> z(n);
 	int l = 0, r = 0;
-	for (int i = 1; i < n; i++) {
-		if (i <= r)
-			z[i] = max(r - i + 1, z[i - l]);
-		while (z[i] + i < n && s[z[i] + i] == s[z[i]])
+	for(int i = 1; i < n; i++) {
+		if(i < r) {
+			z[i] = min(r - i, z[i - l]);
+		}
+		while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
 			z[i]++;
-		if (z[i] + i - 1 > r) {
-			r = z[i] + i - 1;
+		}
+		if(i + z[i] > r) {
 			l = i;
+			r = i + z[i];
 		}
 	}
 	return z;
