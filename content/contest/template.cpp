93f518cdfe7d158bb958224315941f716f6577b6
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2024-02-06 22:36:18 +0200

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -3,10 +3,10 @@
 // #pragma GCC optimize("O3,unroll-loops")
 // #pragma GCC target("sse,sse2,sse3,ssse3,sse4")
 // #pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-
+ 
 // #define _GLIBCXX_DEBUG
 // #define _GLIBCXX_DEBUG_PEDANTIC
-
+ 
 #include <cassert>
 #include <iomanip>
 #include <iostream>
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <functional>
 #include <array>
 #include <numeric>
 #include <queue>
@@ -23,28 +24,49 @@
 // #include <ext/pb_ds/assoc_container.hpp> 
 // #include <ext/pb_ds/tree_policy.hpp>
 // #include <ext/rope>
-
+ 
 // using namespace __gnu_pbds;
 // using namespace __gnu_cxx;
 using namespace std;
-
-
+ 
+ 
 const int MOD = 998244353;
 const long double PI = 3.141592653589793;
 using ll = long long;
 const ll INF = 1e18;
-
+ 
 // #define int ll
+
+// --------> sashko123`s defines:
+
+#define itn int     //Vasya sorry :(
+#define p_b push_back
+#define fi first
+#define se second
+#define pii std::pair<int, int>
+#define oo LLONG_MAX
+#define big INT_MAX
+#define elif else if
+
+int input()
+{
+    int x;
+    cin>>x;
+    return x;
+}
+
+// ----------> end of sashko123`s defines (thank you Vasya <3)
+
 
 // template<typename T>
 // using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-
+ 
 // template<typename K, typename V>
 // using hash_table = cc_hash_table<K, V, hash<K>>;
-
+ 
 template<typename T>
 using graph = vector<vector<T>>;
-
+ 
 template<typename T>
 istream& operator>>(istream& in, vector<T>& a) {
     for (auto& i: a) {
@@ -52,7 +74,7 @@ istream& operator>>(istream& in, vector<T>& a) {
     }
     return in;
 }
-
+ 
 template<typename T>
 ostream& operator<<(ostream& out, vector<T>& a) {
     for (auto& i: a) {
@@ -60,7 +82,7 @@ ostream& operator<<(ostream& out, vector<T>& a) {
     }
     return out;
 }
-
+ 
 ll fast_pow(ll a, ll b, ll mod) {
     if (b == 0)
         return 1;
@@ -69,8 +91,9 @@ ll fast_pow(ll a, ll b, ll mod) {
     }
     ll k = fast_pow(a, b / 2, mod);
     return (1ll * k * k) % mod;
+    
 }
-
+ 
 ll fast_pow(ll a, ll b) {
     if (b == 0)
         return 1;
@@ -80,15 +103,15 @@ ll fast_pow(ll a, ll b) {
     ll k = fast_pow(a, b / 2);
     return (1ll * k * k);
 }
-
+ 
 void solve() {
     
 }
-
+ 
 int32_t main(int32_t argc, const char * argv[]) {
-	cin.tie(0);
-	cout.tie(0);
-	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
     // insert code here...
     int tt= 1;
     // std::cin >> tt;
