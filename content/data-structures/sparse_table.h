7b24112dab1a506a9fcb40162663b4e4e92c56de
VasyaMeR <54406447+VasyaMeR@users.noreply.github.com>
2024-02-27 12:27:51 +0200

    Update sparse_table.h

--- content/data-structures/sparse_table.h
+++ content/data-structures/sparse_table.h
@@ -12,28 +12,31 @@
 #include <array>
 #include <vector>
 
+template<typename T>
 struct sparse_table {
     static const int K = 20;
-    std::array<std::array<T, N>, K> ar;
-    static std::array<int, N + 1> lg;
+    std::array<std::vector<T>, K> ar;
+    std::vector<int> lg;
+    int n;
     
-    sparse_table(int n, const vector<T>& a) {
+    sparse_table(int n, const vector<T>& a): n(n) {
+        lg.resize(n + 1)
         lg[1] = 0;
-        for (int i = 2; i <= N; i++)
+        for (int i = 2; i <= n; i++)
             lg[i] = lg[i >> 1] + 1;
     
-        for (int i = 0; i < n; i++)
-            ar[0][i] = a[i];
+        ar[0] = a;
 
         for (int k = 0; k + 1 < K; k++) {
-            for (int i = 0; i + (1 << k) < N; i++) {
+            ar[k + 1].resize(n);
+            for (int i = 0; i + (1 << k) < n; i++) {
                 ar[k + 1][i] = min(ar[k][i], ar[k][i + (1 << k)]);
             }
         }
     }
 
     T get(int l, int r) {
-        assert(0 <= l < N && l < r && r <= N);
+        assert(0 <= l < n && l < r && r <= n);
         int power = lg[r - l];
         return min(ar[power][l], ar[power][r - (1 << power)]);
     }
