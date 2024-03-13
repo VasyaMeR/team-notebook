0ca58b41817a7d3942648e0b1883d5b1b730db9f
VasyaMer <merenych3@gmail.com>
2024-03-13 15:14:26 +0200

    Update fft.h and nnt.h

--- content/algebra/NNT.h
+++ content/algebra/NNT.h
@@ -8,15 +8,16 @@
  * Status: -
  */
 
-const int root = 31;
-const int root_1 = fast_pow(root, MOD - 2, MOD);
-const int root_pw = 1 << 23;
+#include <vector>
+const int root = 31; // primitive root of module
+const int root_1 = fast_pow(root, MOD - 2, MOD); // (primitive root) ^ -1
+const int root_pw = 1 << 23; // max power of 2 in MOD - 1
  
 inline int inverse(int n, int mod) {
 	return fast_pow(n, mod - 2, mod);
 }
  
-inline void nnt(vector<int> &a, bool invert, int mod) {
+void nnt(std::vector<int> &a, bool invert, int mod) {
     int n = a.size();
  
     for (int i = 1, j = 0; i < n; i++) {
@@ -26,7 +27,7 @@ inline void nnt(vector<int> &a, bool invert, int mod) {
         j ^= bit;
  
         if (i < j)
-            swap(a[i], a[j]);
+            std::swap(a[i], a[j]);
     }
  
     for (int len = 2; len <= n; len <<= 1) {
@@ -40,38 +41,39 @@ inline void nnt(vector<int> &a, bool invert, int mod) {
                 int u = a[i+j], v = (int)(1LL * a[i+j+len/2] * w % mod);
                 a[i+j] = u + v < mod ? u + v : u + v - mod;
                 a[i+j+len/2] = u - v >= 0 ? u - v : u - v + mod;
-                w = (int)(1LL * w * wlen % mod);
+                w = (1ll * w * wlen % mod);
             }
         }
     }
  
     if (invert) {
         int n_1 = inverse(n, mod);
-        for (int & x : a)
-            x = (int)(1LL * x * n_1 % mod);
+        for (int& x : a)
+            x = (1ll * x * n_1 % mod);
     }
 }
  
-inline vector<int> multiply(vector<int> left, vector<int> right) {
-	int n = left.size(), m = right.size();
+std::vector<int> multiply(const std::vector<int>& a, const std::vector<int>& b, int mod) {
+	int n = a.size(), m = b.size();
 	int k = n + m - 1;
 	int l = 1;
 	while (l < k)
 		l <<= 1;
 	
-	vector<int> A(l), B(l);
+	std::vector<int> A(l), B(l);
 	for (int i = 0; i < n; i++) {
-		A[i] = left[i];
+		A[i] = a[i];
 	}
 	for (int i = 0; i < m; i++) {
-		B[i] = right[i];
+		B[i] = b[i];
 	}
  
-	nnt(A, false, MOD);
-	nnt(B, false, MOD);
+	nnt(A, false, mod);
+	nnt(B, false, mod);
 	for (int i = 0; i < B.size(); i++) {
-		A[i] = (1ll * A[i] * B[i]) % MOD;
+		A[i] = (1ll * A[i] * B[i]) % mod;
 	}
-	nnt(A, true, MOD);
+	nnt(A, true, mod);
+	A.resize(k);
 	return A;
 }
--- content/algebra/fft.h
+++ content/algebra/fft.h
@@ -8,60 +8,61 @@
  * Status: -
  */
 
-vector<complex<double>> fft(vector<complex<double>> a, bool inverse = true) {
+#include <vector>
+#include <complex>
+using cd = std::complex<double>;
+
+void fft(std::vector<cd> & a, bool invert) {
     int n = a.size();
-    if (n == 1) {
-        return {a[0]};
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
     }
 
-    int k = n / 2;
-    vector<complex<double>> par(k), nepar(k);
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0)
-            par[i / 2] = a[i];
-        else
-            nepar[i / 2] = a[i];
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
     }
 
-    par = fft(par, inverse);
-    nepar = fft(nepar, inverse);
-    vector<complex<double>> ans(n);
-    for (int i = 0; i < n; i++) {
-        auto x = complex(cos(2 * i * PI / n), sin(2 * i * PI / n));
-        if (inverse)
-            x = complex(cos(2 * i * PI / n), sin(-2 * i * PI / n));
-        ans[i] = par[i % k] + nepar[i % k] * x;
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
     }
-    return ans;
 }
 
-vector<int> mult(vector<int> a, vector<int> b) {
-    int n = a.size();
-    int m = b.size();
-    int k = n + m - 1;
+template<typename T>
+std::vector<T> multiply(const std::vector<T>& a, const std::vector<T>& b) {
+    std::vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
 
-    int l = 1;
-    while (l < k)
-    {
-        l *= 2;
-    }
-    vector<complex<double>> A(l), B(l);
+    fft(fa, false);
+    fft(fb, false);
     for (int i = 0; i < n; i++)
-        A[i] = a[i];
-    for (int j = 0; j < m; j++)
-        B[j] = b[j];
-    A = fft(A, 0);
-    B = fft(B, 0);
+        fa[i] *= fb[i];
+    fft(fa, true);
 
-    for (int i = 0; i < l; i++) {
-        A[i] *= B[i];
-    }
-    A = fft(A, 1);
-
-    vector<int> ans(l);
-    for (int i = 0; i < l; i++) {
-        A[i] /= l;
-        ans[i] = int(A[i].real() + 0.5);
-    }
-    return ans;
+    std::vector<T> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    result.resize(a.size() + b.size() - 1);
+    return result;
 }
