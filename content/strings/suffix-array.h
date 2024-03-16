e2fa0c9d76ebe486eaf6033458fb215dd1430f41
VasyaMer <merenych3@gmail.com>
2024-03-16 12:43:02 +0200

    Add lcp and suffix array

--- content/strings/suffix-array.h
+++ content/strings/suffix-array.h
@@ -4,7 +4,7 @@
  * License: CC0
  * Source: folklore
  * Description: Suffix array will contain integers that represent the starting indexes of the all the suffixes of a given string, after the aforementioned suffixes are sorted.
- * Time: O(N * log2(N)), N - size of string s
+ * Time: O(N * log(N)), N - size of string s
  * Status: -
  */
 
@@ -12,7 +12,7 @@ vector<int> suffix_arrays(string s) {
 	s = s + "$";
 	int n = s.size();
 	
-	vector<int> p(n);
+	std::vector<int> p(n);
 	vector<vector<int>> c(20, vector<int>(n));
 	int alphabet = 256;
 
@@ -30,31 +30,27 @@ vector<int> suffix_arrays(string s) {
 		}
 	};
 
-	auto init_base = [&]() {
-		vector<int> cnt(alphabet);
-		for (int i = 0; i < n; i++) {
-			cnt[s[i]]++;
-		}
-		for (int i = 1; i < alphabet; i++) {
-			cnt[i] += cnt[i - 1];
-		}
-		for (int i = n - 1; i >= 0; i--) {
-			p[cnt[s[i]] - 1] = i;
-			cnt[s[i]]--;
+	vector<int> cnt(alphabet);
+	for (int i = 0; i < n; i++) {
+		cnt[s[i]]++;
+	}
+	for (int i = 1; i < alphabet; i++) {
+		cnt[i] += cnt[i - 1];
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		p[cnt[s[i]] - 1] = i;
+		cnt[s[i]]--;
 
+	}
+	int classes = 0;
+	c[0][p[0]] = classes++;
+	for (int i = 1; i < n; i++) {
+		if (s[p[i]] == s[p[i - 1]]) {
+			c[0][p[i]] = c[0][p[i - 1]];
+		} else {
+			c[0][p[i]] = classes++;
 		}
-		int classes = 0;
-		c[0][p[0]] = classes++;
-		for (int i = 1; i < n; i++) {
-			if (s[p[i]] == s[p[i - 1]]) {
-				c[0][p[i]] = c[0][p[i - 1]];
-			} else {
-				c[0][p[i]] = classes++;
-			}
-		}
-	};
-
-	init_base();
+	}
 
 	for (int k = 0; (1<<k) < n; k++) {
 		vector<int> pn(n), cnt(n);
