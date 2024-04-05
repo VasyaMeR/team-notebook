df4596e19a4088509e1c70f5df7fdce6cf56ca57
VasyaMer <merenych3@gmail.com>
2024-04-05 21:22:19 +0300

    Add half plane intersection

---
+++ content/geometry/half-plane-intersection.h
@@ -0,0 +1,125 @@
+/**
+ * Author: Vasyl Merenych
+ * Date: 2024-04-04
+ * License: CC0
+ * Source: folklore
+ * Description: Finds intersection of half planes. Half-planes are given by a pair of points (a, b) through which a straight line passes in the order of the direction of the vector, so that the plane is to the left of the vector. Builds a voronoi diagram in O(n^2 * \log(n))
+ * Time: O(n * \log(n))
+ * Status: -
+ */
+#include <vector>
+#include <deque>
+using namespace std;
+using ld = long double;
+const ld EPS = 1e-9; 
+
+struct Point { 
+    ld x, y;
+    explicit Point(ld x = 0, ld y = 0) : x(x), y(y) {}
+
+    friend Point operator + (const Point& p, const Point& q) {
+        return Point(p.x + q.x, p.y + q.y); 
+    }
+
+    friend Point operator - (const Point& p, const Point& q) { 
+        return Point(p.x - q.x, p.y - q.y); 
+    }
+
+    friend Point operator * (const Point& p, const ld& k) { 
+        return Point(p.x * k, p.y * k); 
+    } 
+
+    friend ld dot(const Point& p, const Point& q) {
+        return p.x * q.x + p.y * q.y;
+    }
+
+    friend ld cross(const Point& p, const Point& q) { 
+        return p.x * q.y - p.y * q.x; 
+    }
+};
+struct Halfplane { 
+    Point p, pq; 
+    ld angle;
+
+    Halfplane() {}
+    Halfplane(const Point& a, const Point& b) : p(a), pq(b - a) {
+        angle = atan2l(pq.y, pq.x);    
+    }
+
+    bool out(const Point& r) { 
+        return cross(pq, r - p) < -EPS; 
+    }
+
+    bool operator < (const Halfplane& e) const { 
+        return angle < e.angle;
+    } 
+
+    friend Point inter(const Halfplane& s, const Halfplane& t) {
+        ld alpha = cross((t.p - s.p), t.pq) / cross(s.pq, t.pq);
+        return s.p + (s.pq * alpha);
+    }
+};
+
+ld h, w;
+vector<Point> hp_intersect(vector<Halfplane>& H) { 
+
+    Point box[4] = {
+        Point(INF, INF), 
+        Point(-INF, INF), 
+        Point(-INF, -INF), 
+        Point(INF, -INF) 
+    };
+
+    for(int i = 0; i<4; i++) {
+        Halfplane aux(box[i], box[(i+1) % 4]);
+        H.push_back(aux);
+    }
+
+    sort(H.begin(), H.end());
+    deque<Halfplane> dq;
+    int len = 0;
+    for(int i = 0; i < int(H.size()); i++) {
+        while (len > 1 && H[i].out(inter(dq[len-1], dq[len-2]))) {
+            dq.pop_back();
+            --len;
+        }
+
+        while (len > 1 && H[i].out(inter(dq[0], dq[1]))) {
+            dq.pop_front();
+            --len;
+        }
+
+        if (len > 0 && fabsl(cross(H[i].pq, dq[len-1].pq)) < EPS) {
+            if (dot(H[i].pq, dq[len-1].pq) < 0.0)
+                return vector<Point>();
+
+            if (H[i].out(dq[len-1].p)) {
+                dq.pop_back();
+                --len;
+            }
+            else continue;
+        }
+
+        dq.push_back(H[i]);
+        ++len;
+    }
+
+    while (len > 2 && dq[0].out(inter(dq[len-1], dq[len-2]))) {
+        dq.pop_back();
+        --len;
+    }
+
+    while (len > 2 && dq[len-1].out(inter(dq[0], dq[1]))) {
+        dq.pop_front();
+        --len;
+    }
+
+    if (len < 3) return vector<Point>();
+
+    vector<Point> ret(len);
+    for(int i = 0; i+1 < len; i++) {
+        ret[i] = inter(dq[i], dq[i+1]);
+    }
+    ret.back() = inter(dq[len-1], dq[0]);
+    return ret;
+}
